2e53a294dafc6c53520bebe58b63793fb627d332
baltasarq <baltasarq@gmail.com>
2025-10-23 09:41:14 +0200

    First commit

---
+++ src/main/java/com/devbaltasarq/nottakapp/NottakApp.java
@@ -0,0 +1,12 @@
+// NottakApp (c) Baltasar 2025 MIT License <baltasarq@gmail.com>
+
+
+package com.devbaltasarq.nottakapp;
+
+
+public class NottakApp {
+    public static void main(String[] args)
+    {
+        
+    }
+}
