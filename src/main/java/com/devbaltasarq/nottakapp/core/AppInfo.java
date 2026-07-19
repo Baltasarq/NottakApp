037d4e45c266cc719944a8490f8e46a076360a0b
baltasar <baltasarq@gmail.com>
2026-07-19 17:59:25 +0200

    [BUMP] Version

--- src/main/java/com/devbaltasarq/nottakapp/core/AppInfo.java
+++ src/main/java/com/devbaltasarq/nottakapp/core/AppInfo.java
@@ -9,7 +9,7 @@ package com.devbaltasarq.nottakapp.core;
   */
 public class AppInfo {
     public static final String NAME = "NottakApp";
-    public static final String VERSION = "v1.0 20251222";
+    public static final String VERSION = "v1.1 20260711";
     
     public static String TITLE = NAME + " " + VERSION;
 }
